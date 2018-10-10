37a81463c2f8ddfa9eb7cf0140985e0fdd1513d8
aaronsato <2asato@gmail.com>
2018-10-10 13:21:01 -0700

    post controller actions set

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,24 +3,40 @@ class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def index
+    @posts = Post.all
   end
 
   def show
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post, notice: "The post was created!"
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to @post, notice: "Update successful"
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @post.destroy
+    redirect_to root_path, notice: "Post destroyed"
   end
 
 private
