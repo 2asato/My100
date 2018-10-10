331ec62c59cbee0ecff53e2f21d1b0ac99de60fb
aaronsato <2asato@gmail.com>
2018-10-10 11:40:42 -0700

    seeded category names

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.create(name: 'tutorial')
+Category.create(name: 'freecodecamp')
+Category.create(name: 'coderbyte')
+Category.create(name: '100daysofcode')
